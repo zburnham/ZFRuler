dbf583afce7be9761b4dfb87abe1485ea5468152
Zachary Burnham <zburnham@gmail.com>
2013-08-20 20:57:17 -0400

    Adding basic wrappers for Ruler classes. Updating README.md

--- Module.php
+++ Module.php
@@ -1,3 +1,36 @@
 <?php
 
-class Module {}
+namespace ZFRuler;
+
+class Module 
+{
+    
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'Ruler\Context' => function($sm) {
+                    return new Ruler\Context;
+                },
+                'Ruler\Rule' => function($sm) {
+                    return new Ruler\Rule;
+                },
+                'Ruler\RuleBuilder' => function($sm) {
+                    return new Ruler\RuleBuilder;
+                },
+                'Ruler\RuleSet' => function($sm) {
+                    return new Ruler\RuleSet;
+                },
+                'Ruler\Value' => function($sm) {
+                    return new Ruler\Value;
+                },
+                'Ruler\Variable' => function($sm) {
+                    return new Ruler\Variable;
+                },
+                'Ruler\VariableProperty' => function($sm) {
+                    return new Ruler\VariableProperty;
+                }
+            ),
+        );
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 ZFRuler
 =======
 
-Port of Ruler rules engine to a ZF2 module
+Port of Ruler rules engine to a ZF2 module.
+
+Essentially a wrapper around Justin Hileman's Ruler rules engine (https://github.com/bobthecow/Ruler).
+
+Ask the ServiceManager for an instance of Ruler's classes and go for it.  
