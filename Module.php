bf189a5dbdc1a135aae656f8b6a3879f507f2052
Zachary Burnham <zburnham@gmail.com>
2013-08-21 20:55:27 -0400

    Updating some stuff in Module.php regarding autoloading

--- Module.php
+++ Module.php
@@ -2,8 +2,32 @@
 
 namespace ZFRuler;
 
-class Module 
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+
+class Module implements AutoloaderProviderInterface
 {
+    public function getAutoloaderConfig()
+    {
+        return array(
+//            'Zend\Loader\ClassMapAutoloader' => array(
+//                __DIR__ . '/autoload_classmap.php',
+//            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+    
+    public function onBootstrap($e)
+    {
+        // You may not need to do this if you're doing it elsewhere in your
+        // application
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
     
     public function getServiceConfig()
     {
